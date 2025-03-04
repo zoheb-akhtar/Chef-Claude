a63a6a782792003f7c3d32c770fa6e1f787b184a
Zoheb Akhtar <akhtar.zo@northeastern.edu>
2025-03-04 01:28:55 -0500

    Update ai.js

--- ai.js
+++ ai.js
@@ -4,8 +4,6 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `;
 
-console.log(import.meta.env.VITE_HF_ACCESS_TOKEN)
-
 const hf = new HfInference(import.meta.env.VITE_HF_ACCESS_TOKEN);
 
 export async function getRecipeFromMistral(ingredientsArr) {
